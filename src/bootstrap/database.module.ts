e1379cebbc44537928abfbbb5fc6ea0a31614746
veenabellala <veena.b@9and9.com>
2024-09-18 12:57:23 +0530

    Added swagger documentation, created database connection, configured eslint and prettier

---
+++ src/bootstrap/database.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('HOST'),
+        port: Number(configService.get<number>('DB_PORT')),
+        username: configService.get('USERNAME'),
+        password: configService.get('PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
+        synchronize: false,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+import * as packageJson from '../package.json';
 
-async function bootstrap() {
+dotenv.config();
+
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  const host = process.env.HOST || 'localhost';
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle(packageJson.name)
+    .setDescription(packageJson.description)
+    .addServer(`http://${host}:${port}/`)
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('api-docs', app, swaggerDocument);
+  await app.listen(port);
 }
 bootstrap();
